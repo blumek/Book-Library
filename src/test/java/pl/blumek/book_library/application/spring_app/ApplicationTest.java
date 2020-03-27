7639073467965af7858bb521131aad3739936b3d
Blumek <blumczynski.szymon@gmail.com>
2020-03-27 17:07:54 +0100

    Add SpringBoot-test

---
+++ src/test/java/pl/blumek/book_library/application/spring_app/ApplicationTest.java
@@ -0,0 +1,17 @@
+package pl.blumek.book_library.application.spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
