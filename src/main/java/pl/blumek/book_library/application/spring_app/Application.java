394e8294fd0b055f15f1cd4fb72b1d9d8ca40e89
Blumek <blumczynski.szymon@gmail.com>
2020-03-27 16:30:09 +0100

    Add SpringBoot

---
+++ src/main/java/pl/blumek/book_library/application/spring_app/Application.java
@@ -0,0 +1,11 @@
+package pl.blumek.book_library.application.spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
