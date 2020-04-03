8b2813cf34308a542b4f32030e37abda86323304
Blumek <blumczynski.szymon@gmail.com>
2020-04-03 13:22:04 +0200

    Remove redundant access modifier in AuthorReader

--- src/main/java/pl/blumek/book_library/domain/port/AuthorReader.java
+++ src/main/java/pl/blumek/book_library/domain/port/AuthorReader.java
@@ -6,5 +6,5 @@ import java.io.IOException;
 import java.util.List;
 
 public interface AuthorReader {
-    public List<Person> read() throws IOException;
+    List<Person> read() throws IOException;
 }
