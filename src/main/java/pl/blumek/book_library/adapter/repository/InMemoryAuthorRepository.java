c1b6496d4904e96ef9e0b04f851cb9d1609ab156
Blumek <blumczynski.szymon@gmail.com>
2020-04-03 14:47:30 +0200

    Change InMemoryAuthorRepository to extend InMemoryRepository

--- src/main/java/pl/blumek/book_library/adapter/repository/InMemoryAuthorRepository.java
+++ src/main/java/pl/blumek/book_library/adapter/repository/InMemoryAuthorRepository.java
@@ -5,35 +5,26 @@ import pl.blumek.book_library.domain.entity.Person;
 import pl.blumek.book_library.domain.port.AuthorRepository;
 import pl.blumek.book_library.domain.port.IdGenerator;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toMap;
-
-public final class InMemoryAuthorRepository implements AuthorRepository {
-    private final Map<String, Person> inMemoryDb;
-    private final IdGenerator idGenerator;
-
-    public InMemoryAuthorRepository(IdGenerator idGenerator, Person... people) {
-        this(idGenerator, Arrays.stream(people));
+public final class InMemoryAuthorRepository extends InMemoryRepository<Person> implements AuthorRepository {
+    public InMemoryAuthorRepository(IdGenerator idGenerator, Person... t) {
+        super(idGenerator, t);
     }
 
-    public InMemoryAuthorRepository(IdGenerator idGenerator, List<Person> people) {
-        this(idGenerator, people.stream());
+    public InMemoryAuthorRepository(IdGenerator idGenerator, List<Person> t) {
+        super(idGenerator, t);
     }
 
-    public InMemoryAuthorRepository(IdGenerator idGenerator, Stream<Person> peopleStream) {
-        this.idGenerator = idGenerator;
-        this.inMemoryDb = peopleStream
-                .map(this::authorWithAssignedId)
-                .collect(toMap(Person::getId, author -> author));
+    public InMemoryAuthorRepository(IdGenerator idGenerator, Stream<Person> personStream) {
+        super(idGenerator, personStream);
     }
 
-    private Person authorWithAssignedId(Person author) {
-        return author.toBuilder()
-                .id(getId(author))
+    @Override
+    Person entityWithAssignedId(Person person) {
+        return person.toBuilder()
+                .id(getId(person))
                 .build();
     }
 
@@ -41,12 +32,13 @@ public final class InMemoryAuthorRepository implements AuthorRepository {
         return hasId(person) ? person.getId() : generateId();
     }
 
-    private String generateId() {
-        return idGenerator.generate();
-    }
-
     private boolean hasId(Person person) {
         return person.getId() != null && !person.getId().isEmpty();
+    }
+
+    @Override
+    protected String getEntityId(Person person) {
+        return person.getId();
     }
 
     @Override
