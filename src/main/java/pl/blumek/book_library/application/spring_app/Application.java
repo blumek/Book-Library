ddd8b87655ae12a8d0b2a203d1240817a7e87f69
Blumek <blumczynski.szymon@gmail.com>
2020-04-03 11:43:10 +0200

    Add google books datasoruce to CLI

--- src/main/java/pl/blumek/book_library/application/spring_app/Application.java
+++ src/main/java/pl/blumek/book_library/application/spring_app/Application.java
@@ -1,6 +1,5 @@
 package pl.blumek.book_library.application.spring_app;
 
-import com.google.common.io.Files;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,8 +7,10 @@ import picocli.CommandLine;
 import pl.blumek.book_library.adapter.id_generator.UUIDGenerator;
 import pl.blumek.book_library.adapter.reader.JsonFileAuthorsFromBookGoogleReader;
 import pl.blumek.book_library.adapter.reader.JsonFileBookGoogleReader;
+import pl.blumek.book_library.adapter.repository.GoogleBooksBookRepository;
 import pl.blumek.book_library.adapter.repository.InMemoryAuthorRepository;
 import pl.blumek.book_library.adapter.repository.InMemoryBookRepository;
+import pl.blumek.book_library.application.spring_app.client.GoogleBooksClient;
 import pl.blumek.book_library.application.spring_app.command.DataSourceCommand;
 import pl.blumek.book_library.config.Config;
 import pl.blumek.book_library.domain.entity.Book;
@@ -21,44 +22,69 @@ import pl.blumek.book_library.domain.port.BookRepository;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.toList;
+
 @SpringBootApplication
 public class Application {
-    private static final String JSON_EXTENSION = "json";
-
     private static BookRepository bookRepository;
     private static AuthorRepository authorRepository;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
         handleArgs(args);
         SpringApplication.run(Application.class, args);
     }
 
-    private static void handleArgs(String[] args) throws IOException {
+    private static void handleArgs(String[] args) throws IOException, GeneralSecurityException {
         DataSourceCommand dataSourceCommand = new DataSourceCommand();
-        CommandLine commandLine = new CommandLine(dataSourceCommand);
-        commandLine.parseArgs(args);
+        parseArgs(args, dataSourceCommand);
 
         Optional<File> jsonFile = dataSourceCommand.getJsonFile();
-        if (jsonFile.isPresent()) {
+        if (dataSourceCommand.useGoogleBooks()) {
+            handleGoogleBooks();
+        } else if (jsonFile.isPresent()) {
             handleJsonFile(jsonFile.get());
         } else {
             handleDefault();
         }
     }
 
-    private static void handleJsonFile(File jsonFile) throws IOException {
-        if (!Files.getFileExtension(jsonFile.getName()).equals(JSON_EXTENSION))
-            throw new IOException("Provided file is not in JSON format");
+    private static void parseArgs(String[] args, DataSourceCommand dataSourceCommand) {
+        CommandLine commandLine = new CommandLine(dataSourceCommand);
+        commandLine.parseArgs(args);
+    }
 
+    private static void handleGoogleBooks() throws GeneralSecurityException, IOException {
+        bookRepository = new GoogleBooksBookRepository(GoogleBooksClient.client());
+
+        List<Book> sampleBooks = bookRepository.findAllByCategoryName("Computers");
+        authorRepository = new InMemoryAuthorRepository(UUIDGenerator.create(), getAuthorsFromBooks(sampleBooks));
+    }
+
+    private static List<Person> getAuthorsFromBooks(List<Book> sampleBooks) {
+        return sampleBooks.stream()
+                .map(Book::getAuthors)
+                .flatMap(Collection::stream)
+                .collect(toList());
+    }
+
+    private static void handleJsonFile(File jsonFile) throws IOException {
+        bookRepository = new InMemoryBookRepository(UUIDGenerator.create(), getBooksFromJson(jsonFile));
+        authorRepository = new InMemoryAuthorRepository(UUIDGenerator.create(), getAuthorsFromJson(jsonFile));
+    }
+
+    private static List<Book> getBooksFromJson(File jsonFile) throws IOException {
         BookReader bookReader = new JsonFileBookGoogleReader(jsonFile);
-        List<Book> books = bookReader.read();
-        bookRepository = new InMemoryBookRepository(UUIDGenerator.create(), books);
+        return bookReader.read();
+    }
+
+    private static List<Person> getAuthorsFromJson(File jsonFile) throws IOException {
         AuthorReader authorReader = new JsonFileAuthorsFromBookGoogleReader(jsonFile);
-        List<Person> authors = authorReader.read();
-        authorRepository = new InMemoryAuthorRepository(UUIDGenerator.create(), authors);
+        return authorReader.read();
     }
 
     private static void handleDefault() {
--- src/main/java/pl/blumek/book_library/application/spring_app/command/DataSourceCommand.java
+++ src/main/java/pl/blumek/book_library/application/spring_app/command/DataSourceCommand.java
@@ -1,9 +1,11 @@
 package pl.blumek.book_library.application.spring_app.command;
 
+import com.google.common.io.Files;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @Command(
@@ -11,10 +13,24 @@ import java.util.Optional;
         description = "Select data source for application"
 )
 public class DataSourceCommand {
-    @Option(names = { "-jf", "--jsonFile" })
     private File jsonFile;
+
+    @Option(names = { "-gb", "--googleBooks" })
+    private boolean useGoogleBooks;
+
+    @Option(names = { "-jf", "--jsonFile" })
+    void setJsonFile(File file) throws IOException {
+        if (!Files.getFileExtension(file.getName()).equals("json"))
+            throw new IOException("Provided file is not in JSON format");
+
+        this.jsonFile = file;
+    }
 
     public Optional<File> getJsonFile() {
         return Optional.ofNullable(jsonFile);
+    }
+
+    public boolean useGoogleBooks() {
+        return useGoogleBooks;
     }
 }
